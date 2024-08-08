03b914cdd1eba736d952b85830e209e52690ea93
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-08 16:49:32 +0300

    done

---
+++ src/app/loggedin/layout.tsx
@@ -0,0 +1,1 @@
+import { ConvexClientProvider } from "../ConvexClientProvider";
---
+++ src/app/loggedin/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function LoggedIn() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>LogedIn</h1>
+    </main>
+  );
+}
