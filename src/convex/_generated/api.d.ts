9de03f3ef4eb82d7038c28527b1ccd5e7b45257f
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-08 16:09:31 +0300

    not respond

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as data_tasks from "../data/tasks.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -23,7 +24,9 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  "data/tasks": typeof data_tasks;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
