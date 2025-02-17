07675bbc3bc5f10040b6e02b8d773932d3fac009
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-17 14:43:39 +0200

    done

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -8,6 +8,7 @@ subscriptionRouter.post('/', (req, res) => res.send({ title: 'CREATE subscriptio
 subscriptionRouter.put('/', (req, res) => res.send({ title: 'UPDATE subscription'}));
 subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription'}));
 subscriptionRouter.get('/user/:id', (req, res) => res.send({ title: 'GET all user subscriptions'}));
-subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions'}));
+subscriptionRouter.get('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription'}));
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({ title: 'GET upcoming renewals'}));
 
 export default subscriptionRouter;
