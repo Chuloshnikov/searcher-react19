4417642ef53840d6cee2cb3030205642bc48ef15
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-21 16:37:17 +0200

    arcjet added

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
 import connectToDatabase from './database/mongodb.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware); //mult req protection
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
