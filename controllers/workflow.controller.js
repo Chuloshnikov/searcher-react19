44e3bdd71a17ea1b009d7a5fb5ee178da9ec3ddc
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-21 19:49:29 +0200

    workflow for subscriptions added

---
+++ controllers/workflow.controller.js
@@ -0,0 +1,52 @@
+import dayjs from 'dayjs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { serve } = require('@upstash/workflow/express'); //works correctly only with common 
+import Subscription from '../models/subscription.model.js';
+
+const REMINDERS = [7, 5, 3, 1];
+
+export const sendReminders = serve( async (context) => {
+    const { subscriptionId } = context.requestPayload;
+    const subscription = await fetchSubscription(context, subscriptionId);
+
+    if (!subscription || subscription.status !== 'active')  return;
+
+    const renewalDate = dayjs(subscription.renewalDate);
+
+    if (renewalDate.isBefore(dayjs())) {
+        console.log(`Renewal date has passed for subscription ${subscription}. Stopping workflow.`);
+        return;
+    }
+
+    for (const daysBefore of REMINDERS) {
+        const reminderDate = renewalDate.subtract(daysBefore, 'day');
+        
+        if(reminderDate.isAfter(dayjs())) {
+            await sleepUntilReminder(context, `Reminder ${daysBefore} days before`, reminderDate);
+        }
+
+        if (dayjs().isSame(reminderDate, 'day')) {
+            await triggerReminder(context, `${daysBefore} days before reminder`, subscription);
+        }
+    }
+});
+
+const fetchSubscription = async (context, subscriptionId) => {
+    return await context.run('get subscription', async () => {
+        return Subscription.findById(subscriptionId).populate('user', 'user email');
+    })
+}
+
+
+const sleepUntilReminder = async (context, label, date) => {
+    console.log(`Sleeping until ${label} reminder at ${date}`);
+    await context.sleepUntil(label, date.toDate());
+}
+
+const triggerReminder = async (context, label) => {
+    return await context.run(label, () => {
+        console.log(`Triggering ${label} reminder`);
+        //Send email or SMS or post bird
+    });
+}
